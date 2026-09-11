0020486a1b472c82b5368e269b2223e00c85edfa
YOUR_GITHUB_USERNAME <prospereze12345@gmail.com>
2026-09-11 22:01:04 +0100

    Add Google Search Console verification

--- orphanage/urls.py
+++ orphanage/urls.py
@@ -1,29 +1,23 @@
-"""
-URL configuration for orphanage project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
 from django.urls import path, include
 
 from website.sitemap import StaticViewSitemap
 from website.seo_views import robots_txt
 
+
+def google_verification(request):
+    return HttpResponse(
+        "google-site-verification: google19468042a663e340.html",
+        content_type="text/plain",
+    )
+
+
 sitemaps = {
     "static": StaticViewSitemap,
 }
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -38,5 +32,10 @@ urlpatterns = [
     ),
 
     path("robots.txt", robots_txt, name="robots_txt"),
-]
 
+    path(
+        "google19468042a663e340.html",
+        google_verification,
+        name="google_verification",
+    ),
+]
